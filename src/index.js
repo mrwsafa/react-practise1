708eeacd7151c9a97dabaa4c5f94dde85dd5a26d
mrwsafa <58708072+mrwsafa@users.noreply.github.com>
2021-06-01 09:33:15 +0300

    ApprovalCard added

--- src/index.js
+++ src/index.js
@@ -2,28 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        text="Nice Blog"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 3:45PM"
-        text="Nice"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="John"
-        timeAgo="Today at 2:45PM"
-        text="Blog"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          text="Nice Blog"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 3:45PM"
+          text="Nice"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="John"
+          timeAgo="Today at 2:45PM"
+          text="Blog"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
